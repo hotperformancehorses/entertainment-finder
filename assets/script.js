4d75a95f2afba442b14523e9853ca86b689bd6c0
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-07-07 14:18:01 -0400

    added a background image for the top section of the page and added a navbar

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,3 @@
-const apiKeyTm= "d8456d6d5c57d78561ea4588cc7a1a54";
+const apiKeyTm = "d8456d6d5c57d78561ea4588cc7a1a54";
 
-const requestURL= "https://api.themoviedb.org/3/movie/11?api_key=" + apiKeyTm;
+const requestURL = "https://api.themoviedb.org/3/movie/11?api_key=" + apiKeyTm;
