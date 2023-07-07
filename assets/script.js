0835c9d7ab33531d9421142a08671f942f918e76
Lilibeth Mejia <lilibethmejia@Lilibeths-Air.attlocal.net>
2023-07-07 12:49:08 -0500

    added comments to API

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Lily's API Key for TMDB
 const apiKeyTm= "d8456d6d5c57d78561ea4588cc7a1a54";
 
+// Base url for API key
 const requestURL= "https://api.themoviedb.org/3/movie/11?api_key=" + apiKeyTm;
