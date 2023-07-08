b10be2ade75a58b9755a42613b18439b0fb9d779
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-07-08 10:53:59 -0400

    added bulma classes to the background image

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// This fucntion controls the sliders
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -28,3 +29,25 @@ const apiKeyTm = "d8456d6d5c57d78561ea4588cc7a1a54";
 
 // Base url for API key
 const requestURL = "https://api.themoviedb.org/3/movie/11?api_key=" + apiKeyTm;
+
+// Navbar functionality
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle("is-active");
+      $target.classList.toggle("is-active");
+    });
+  });
+});
